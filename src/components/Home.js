f02415edda667ab902762dbccd99269a4db88e5d
joelwaiman <waimanjoel@gmail.com>
2023-06-08 16:43:07 -0300

    avatar y links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
 import './Home.css'
+import image from '../assets/avatar.png'
 
 const Home = () => {
-    return(
-        <div className="titleHead">
-            <h1>Hi, I'm <strong className="textHome"> Joel <br/> Front End Developer <br/> </strong> It's nice you're here</h1>
-        </div>
+    return (
+        <section className="homeSide">
+            <div className="avatar">
+                <img className="img" src={image} alt="Chico con gafas sonriendo"/>
+            </div>
+            <div className="titleHead">
+                <h1>Hi, I'm <strong className="textHome"> Joel <br /> Front End Developer <br /> </strong> It's nice you're here</h1>
+            </div>
+        </section>
     )
 }
 
--- src/components/Projetcs.js
+++ src/components/Projetcs.js
@@ -1,33 +1,36 @@
 import React from "react";
 import './Projects.css';
+import { Link } from "react-router-dom";
 
 const Projects = () => {
-    const project = [
-        { id: 0, name: 'Clon MeLi' },
-        { id: 1, name: 'E-Commerce' },
-    ]
+  const project = [
+    { id: 0, name: 'Clon MeLi', link: 'https://me-li-clon.vercel.app/' },
+    { id: 1, name: 'E-Commerce'},
+  ]
 
 
-    return (
-        <section className="projectCard">
-            {project.map((card) => {
-                return <div class="card-container">
-                <div class="card">
-                <div class="front-content">
-                  <p>{card.name}</p>
-                </div>
-                <div class="content">
-                  <p class="heading">{card.name}</p>
-                  <p class="bodyCard">
-                    Lorem ipsum dolor sit amet, consectetur adipii
-                    voluptas ten mollitia pariatur odit
-                  </p>
-                </div>
+  return (
+    <section className="projectCard">
+      {project.map((card) => {
+        return <div class="card-container">
+          <Link className="link" to={card.link}>
+          <div class="card">
+            <div class="front-content">
+              <p>{card.name}</p>
+            </div>
+              <div class="content">
+                <p class="heading">{card.name}</p>
+                <p class="bodyCard">
+                  Lorem ipsum dolor sit amet, consectetur adipii
+                  voluptas ten mollitia pariatur odit
+                </p>
               </div>
-              </div>
-            })}
-        </section>
-    )
+          </div>
+          </Link>
+        </div>
+      })}
+    </section>
+  )
 }
 
 export default Projects;
