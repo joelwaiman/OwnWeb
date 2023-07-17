03bd685b5b685018388daf23d974d56aac12a5b0
joelwaiman <waimanjoel@gmail.com>
2023-07-17 17:19:56 -0300

    add tw

--- src/components/Projetcs.js
+++ src/components/Projetcs.js
@@ -6,6 +6,7 @@ const Projects = () => {
   const project = [
     { id: 0, name: 'Clon MeLi', link: 'https://me-li-clon.vercel.app/', description:'Clon de conocido E-commerce. Ruteo y filtrado, tanto de categoria, como producto individual. Consumiendo API de FakeStore' },
     { id: 1, name: 'E-Commerce', link:'https://fantastic-snickerdoodle-df7706.netlify.app/', description: 'Proyecto de fin de curso, E-commerce completo creado con ReactJS. Uso de Hooks, States, Context y mas. Firebase como base de datos'},
+    { id: 2, name: 'Clon Twitter', link:'https://twitter-clone-joelwaiman.vercel.app/', description: 'Proyecto hecho en React, con el objeto de crear una UI lo mas asemejada posible a la original. Control de estados, localStorage'}
   ]
 
 
