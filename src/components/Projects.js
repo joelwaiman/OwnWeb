9d66f738109c6d23edba51003389d189b7041b24
joelwaiman <waimanjoel@gmail.com>
2024-01-24 16:07:38 -0300

    add project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,13 +5,16 @@ import tw from '../assets/tw.jpg';
 import meli from '../assets/meli.jpg';
 import chonkee from '../assets/chonkees.jpg'
 import midubooks from '../assets/midubooks.jpg'
+import smash from '../assets/smash.png'
 
 const Projects = () => {
   const project = [
     { id: 0, name: 'Símil MeLi', img: meli, link: 'https://me-li-clon.vercel.app/', description: 'Clon de conocido E-commerce. Ruteo y filtrado, tanto de categoria, como producto individual. Consumiendo API de FakeStore.' },
     { id: 1, name: 'E-Commerce', img: chonkee, link: 'https://ecommerce-react-ten-phi.vercel.app/', description: 'Proyecto de fin de curso, E-commerce completo creado con ReactJS. Uso de Hooks, States, Context y mas. Firebase como base de datos.' },
     { id: 2, name: 'Clon Twitter', img: tw, link: 'https://twitter-clone-lime.vercel.app', description: 'Proyecto hecho en ReactJS, con el objeto de crear una UI lo mas asemejada posible a la original. Control de estados, localStorage.' },
-    { id: 3, name: 'Midubooks', img: midubooks, link: 'https://midubooks.vercel.app/', description: 'Proyecto hecho en NextJS, simple organizador de libros, con filtrado por categorias y con la opción de poder agregar libros a favoritos.' }
+    { id: 3, name: 'Midubooks', img: midubooks, link: 'https://midubooks.vercel.app/', description: 'Proyecto hecho en NextJS, simple organizador de libros, con filtrado por categorias y con la opción de poder agregar libros a favoritos.' },
+    { id: 4, name: 'Smash Burguers', img: smash, link: 'https://burguers.vercel.app/', description: 'Hamburguesería en proceso de creación para cliente local, con el fin de mostrar su trabajo y productos disponibles.' },
+
   ]
 
 
