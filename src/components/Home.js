9cdf473ff84327a817d3e8e6dbda71382ba2280d
joelwaiman <waimanjoel@gmail.com>
2024-06-30 21:03:06 -0300

    fix styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 import styles from './Image.module.css'
 
 
-import image from '../assets/avatar.png'
+import image from '../assets/avatar.webp'
 import icons from '../assets/icons'
 
 import iconsStack from '../assets/iconsStack'
@@ -14,29 +14,32 @@ const Home = () => {
         <section className="animate-fade animate-delay-700 flex flex-col-reverse justify-center md:w-[80%] w-full m-auto min-h-screen"
             id="home"
         >
-            <div className="flex items-center justify-center w-[90%] md:w-[90%] mx-auto py-12 md:order-1 order-1">
+            <header className="flex items-center justify-center w-[90%] md:w-[90%] mx-auto py-12 md:order-1 order-1">
                 <h1 className="text-6xl text-[#EEEEEE] font-bold inline-block">Bienvenidos</h1>
                 <hr className="hidden md:flex md:flex-grow md:ml-4 md:bg-[#EEEEEE] md:border-0 md:h-[1px]" />
-            </div>
-            <div className="flex flex-col md:flex-row md:justify-center justify-evenly items-center" >
-                <div className="flex justify-center md:w-[45%] w-1/2">
+            </header>
+            <section className="flex flex-col md:flex-row md:justify-center justify-evenly items-center" >
+                <article className="flex justify-center md:w-[45%] w-1/2 animate-fade-right animate-delay-700">
                     <img className={styles.image}
                         src={image}
                         alt="Chico con gafas sonriendo" />
-                </div>
-                <div className="animate-fade-left animate-delay-700 flex flex-col items-center bg-[rgba(255,255,255,0.06)] backdrop-blur-[20px] rounded-2xl shadow-[rgba(0,0,0,0.2)] p-5 md:items-start text-center md:text-start w-[90%] md:w-1/2 text-xl text-slate-100">
+                </article>
+                <article className="animate-fade-left animate-delay-700 flex flex-col items-center bg-[rgba(255,255,255,0.06)] backdrop-blur-[20px] rounded-2xl shadow-[rgba(0,0,0,0.2)] p-5 md:items-start text-center md:text-start w-[90%] md:w-1/2 text-xl text-slate-100">
                     <p className="text-3xl">Hola, soy
                         <strong className="bg-gradient-to-tr from-[#76ABAE] to-[#EEEEEE] text-transparent bg-clip-text"> Joel
                             <br /> Front End Developer <br />
                         </strong> Es un placer que estés aqui.</p>
                     <div
-                        className="flex justify-between w-3/4 text-3xl text-[#76ABAE] py-5 animate-fade">
+                        className="flex justify-between w-full md:w-3/4 text-3xl text-[#76ABAE] py-5 animate-fade">
                         {iconsStack.map((icns) => {
-                            return <p className="hover:scale-110 transition-all duration-300"
+                            return <div className={styles.icons_container}>
+                            <span className={styles.icons}
                                 key={icns.id}
                                 title={icns.title}>
                                 {icns.name}
-                            </p>
+                            </span>
+                            <p className={styles.icons_name}>{icns.title}</p>
+                            </div>
                         })}
                     </div>
                     <p>Mi enfoque en el desarrollo front-end se centra en la creación de buenas experiencias visuales. Trabajo activamente en la mejora de la estética y la usabilidad del sitio.</p>
@@ -53,8 +56,8 @@ const Home = () => {
                             </a>
                         })}
                     </div>
-                </div>
-            </div>
+                </article>
+            </section>
         </section>
     )
 }
