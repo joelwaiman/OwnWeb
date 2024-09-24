bd96b06823c9c5a5e51b2db36919e5c5ede74a33
joelwaiman <waimanjoel@gmail.com>
2024-09-24 17:10:45 -0300

    fix home

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,8 +11,8 @@ const Home = () => {
         <section className="animate-fade animate-delay-700 flex flex-col justify-center items-center min-h-screen m-auto lg:w-[80%] max-w-[95%] text-text gap-10"
             id="home">
 
-            <div className="xl:absolute top-[10%] left-[30%] h-[30%] w-[30%] bg-primary opacity-30 -z-10 rounded-full blur-[90px]" />
-            <div className="xl:absolute top-[40%] right-[30%] h-[30%] w-[20%] bg-primary opacity-40 -z-10 rounded-full blur-[90px] animate-pulse animate-infinite animate-duration-[5000ms]" />
+            <div className="lg:absolute top-[10%] left-[30%] h-[30%] w-[30%] bg-primary opacity-30 -z-10 rounded-full blur-[90px] animate-pulse animate-infinite animate-duration-[7000ms]" />
+            <div className="lg:absolute top-[40%] right-[30%] h-[30%] w-[20%] bg-primary opacity-40 -z-10 rounded-full blur-[90px] animate-pulse animate-infinite animate-duration-[5000ms]" />
 
             {/* TEXTO BACKGROUND */}
 
@@ -26,42 +26,43 @@ const Home = () => {
                 WELCOME
             </h1>
 
-            <div className="px-4 md:px-6 z-10 max-w-[80%]">
+            <div className="px-4 md:px-6 max-w-[80%]">
                 <div className="flex flex-col lg:flex-row items-center gap-6 md:gap-12">
                     <div className="flex justify-center w-full md:w-3/4">
                         <img
-                            alt="Portfolio image"
-                            className="w-1/2 lg:w-4/5 aspect-square overflow-hidden rounded-xl object-cover object-center"
+                            alt="Me"
+                            className="w-1/2 lg:w-4/5 aspect-square overflow-hidden rounded-xl object-cover object-center border-4 border-gray-400/20 lg:rotate-3 lg:hover:rotate-0 transition-all duration-500 lg:hover:border-secondary lg:hover:shadow-[0_0_10px_0_rgba(197,186,196,0.5)]"
                             height="600"
                             src={image}
                             width="600"
                         />
                     </div>
-                    <article className="animate-fade-left animate-delay-700 flex flex-col text-text items-center bg-glassmorph backdrop-blur-[20px] rounded-2xl shadow-[rgba(0,0,0,0.2)] p-5 text-center w-full lg:w-3/4">
-                        <p className="text-4xl">Hola, soy
-                            <strong className="bg-gradient-to-tr from-primary to-secondary text-transparent bg-clip-text text-5xl"> Joel
+                    <article className="animate-fade-left animate-delay-700 flex flex-col text-text items-center lg:items-start lg:text-start bg-glassmorph backdrop-blur-[20px] rounded-2xl p-5 text-center w-full lg:w-3/4">
+                        <p className="text-3xl font-medium">Hola, soy
+                            <strong className="bg-gradient-to-tr font-bold from-primary to-secondary text-transparent bg-clip-text text-5xl"> Joel
                                 <br /> Front End Developer <br />
-                            </strong> Es un placer que estés aqui.</p>
+                            </strong> Es un placer que estés aqui.
+                        </p>
                         <div
-                            className="flex w-full justify-between md:w-3/4 text-3xl text-secondary py-5 animate-fade">
+                            className="flex flex-wrap w-full justify-center lg:justify-start gap-4 md:w-3/4 text-3xl text-secondary py-5 animate-fade">
                             {iconsStack.map((icns) => {
-                                return <div className="min-w-fit">
+                                return <div className="flex flex-col items-center min-w-fit hover:scale-[1.1] transition-all duration-200">
                                     <span className=""
                                         key={icns.id}
                                         title={icns.title}>
                                         {icns.name}
                                     </span>
-                                    <p className="hidden ">{icns.title}</p>
+                                    <p className="text-sm">{icns.title}</p>
                                 </div>
                             })}
                         </div>
-                        <p>Mi enfoque en el desarrollo front-end se centra en la creación de buenas experiencias visuales. Trabajo activamente en la mejora de la estética y la usabilidad del sitio.</p>
-                        <div className="flex justify-between items-stretch md:w-1/2 w-3/4 py-5">
+                        <p className="text-xl">Mi enfoque en el desarrollo front-end se centra en la creación de buenas experiencias visuales. Trabajo activamente en la mejora de la estética y la usabilidad del sitio.</p>
+                        <div className="flex gap-4 items-stretch md:w-1/2 w-3/4 py-5 text-xl">
                             Contactame:
                             {icons.map((icon) => {
                                 return <a
                                     key={icon.id}
-                                    className="text-3xl text-primary hover:scale-110 transition-all duration-300"
+                                    className="text-3xl text-secondary lg:text-dark hover:scale-110 transition-all duration-300"
                                     href={icon.route}
                                     target="_blank"
                                     rel="noopener noreferrer">
