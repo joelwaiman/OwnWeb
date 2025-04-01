3da0accc44e19260bc471f1b49527493301588cf
joelwaiman <waimanjoel@gmail.com>
2025-04-01 14:04:24 -0300

    Styles card

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -22,28 +22,39 @@ export default function ProjectCard({ project }: ProjectCardProps) {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.2 }}
       viewport={{ once: true }}
-      className="bg-light flex justify-between flex-col w-full lg:w-[48%] p-6 rounded-md shadow-md"
+      className="bg-light w-full lg:w-[48%] p-6 rounded-md shadow-md relative overflow-hidden group z-0"
     >
-      <Link 
-        className="flex flex-col xl:flex-row justify-between gap-4 items-start xl:items-center mb-4"
+      {/* Círculo que se expande en hover */}
+      <div className="absolute h-[5em] w-[5em] -bottom-[2.5em] -left-[2.5em] rounded-full bg-primary group-hover:scale-[2000%] duration-500 z-[1]"></div>
+
+      <Link
+        className="flex flex-col h-full justify-between gap-4 items-start xl:items-center mb-4 relative z-10"
         href={project.link}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <h3 className="text-4xl xl:text-6xl text-tertiary font-bevan">{project.title}</h3>
-        <span className="text-tertiary text-3xl md:text-5xl font-bevan">{project.year}</span>
+        <div className="flex flex-wrap justify-between items-center gap-4 w-full">
+          <h3 className="text-4xl xl:text-6xl text-tertiary font-bevan group-hover:text-light duration-500">
+            {project.title}
+          </h3>
+          <span className="text-tertiary text-3xl md:text-5xl font-bevan group-hover:text-light duration-500">
+            {project.year}
+          </span>
+        </div>
+        <p className="text-dark font-hind text-lg md:text-2xl mb-4 group-hover:text-light duration-500">
+          {project.description}
+        </p>
+        <div className="flex flex-wrap w-full gap-2 md:justify-end">
+          {project.techs.map((btn, i) => (
+            <motion.button
+              key={i}
+              className="bg-tertiary text-light px-3 py-2 rounded-md text-sm md:text-md group-hover:bg-dark group-hover:text-light duration-500"
+            >
+              {btn}
+            </motion.button>
+          ))}
+        </div>
       </Link>
-      <p className="text-dark font-hind text-lg md:text-2xl mb-4">{project.description}</p>
-      <div className="flex flex-wrap gap-2 justify-start md:justify-end">
-        {project.techs.map((btn, i) => (
-          <motion.button
-            key={i}
-              className="bg-tertiary text-light px-3 py-2 rounded-md text-sm md:text-md"
-          >
-            {btn}
-          </motion.button>
-        ))}
-      </div>
     </motion.div>
   )
 }
